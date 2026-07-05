b486f90a05c369e5881f0f32ba30226d71995c9d
fdr <dongri.fu@dexintec.com>
2026-07-05 19:11:03 +0800

    update freedb to 1.0.0

--- Casks/freedb.rb
+++ Casks/freedb.rb
@@ -1,10 +1,10 @@
 cask "freedb" do
   version "1.0.0"
-  sha256 "32fa6822f0744d910b633c5e523c8b057f3b440f70f2844c277ea6a50970dffe"
+  sha256 "d2690f90fc1015ae792621139b07d73137f074ace17bdb1dc667741deaffbdd6"
 
   url "https://github.com/fudongri/freeDB/releases/download/#{version}/FreeDB.dmg"
   name "FreeDB"
-  desc "Lightweight cross-platform database client for MySQL & PostgreSQL"
+  desc "Lightweight cross-platform database client for MySQL, PostgreSQL, and MongoDB"
   homepage "https://github.com/fudongri/freeDB"
 
   app "FreeDB.app"
