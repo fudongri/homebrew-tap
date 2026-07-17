eeacc00abd809535d53ace925819b5aa31388c51
fdr <dongri.fu@dexintec.com>
2026-07-17 10:09:58 +0800

    update freedb to 1.1.2

--- Casks/freedb.rb
+++ Casks/freedb.rb
@@ -1,6 +1,6 @@
 cask "freedb" do
   version "1.1.2"
-  sha256 "ee458fde9037b62256a66d6c0b6d8d66c050d1a5aaa6db440b6a14a8a01c0b68"
+  sha256 "1d21008cc4b4780c795765001d13223500cda3510a4f0185c8656c9248af693a"
 
   url "https://github.com/fudongri/freeDB/releases/download/#{version}/FreeDB.dmg"
   name "FreeDB"
